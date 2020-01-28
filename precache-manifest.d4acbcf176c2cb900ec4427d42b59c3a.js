3942a6a203a36d25663e4de44a59d26001571ae0
Kripa <kripabairagi0047@gmail.com>
2020-01-28 07:55:47 -0800

    Updates

---
+++ precache-manifest.d4acbcf176c2cb900ec4427d42b59c3a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c79de228576a010fd764e7fafe76be9",
+    "url": "/startMaster/index.html"
+  },
+  {
+    "revision": "ab0f4f52fd93bacd886a",
+    "url": "/startMaster/static/css/main.061a3c05.chunk.css"
+  },
+  {
+    "revision": "be4c08a6490bc3a843b6",
+    "url": "/startMaster/static/js/2.be92db56.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/startMaster/static/js/2.be92db56.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ab0f4f52fd93bacd886a",
+    "url": "/startMaster/static/js/main.672e41ee.chunk.js"
+  },
+  {
+    "revision": "4a8245f9dfa450d830a5",
+    "url": "/startMaster/static/js/runtime-main.588feebe.js"
+  },
+  {
+    "revision": "84f50a53f62bb77f2f92dfbbd10b7b3c",
+    "url": "/startMaster/static/media/Star-Wars.84f50a53.jpg"
+  },
+  {
+    "revision": "18c3da0da13611f1194277bd15199084",
+    "url": "/startMaster/static/media/bb8Loader.18c3da0d.gif"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.01078deb271fa52f1de2ca0073a837c2.js"
+  "/startMaster/precache-manifest.d4acbcf176c2cb900ec4427d42b59c3a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/startMaster/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
